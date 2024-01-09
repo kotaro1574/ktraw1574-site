f07d55b94854fb01513aec8d23b4f58dcdb7f982
kotaro1574 <shake1574@gmail.com>
2024-01-09 15:26:20 -0800

    create: とりあえず適当に

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image"
+
 export default function IndexPage() {
   return (
-    <section>
+    <main>
       <div className="relative h-screen w-screen">
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
@@ -11,6 +13,55 @@ export default function IndexPage() {
         />
         <div className="absolute inset-0 bg-black/50" />
       </div>
-    </section>
+      <div className="container mx-auto px-4 py-6">
+        <div className="grid gap-6">
+          <section className="grid gap-4">
+            <h1 className="text-4xl font-bold text-white">ABOUT</h1>
+            <div className="flex gap-4">
+              <div className="w-[200px]">
+                <Image
+                  alt="me"
+                  src="/about-me.jpg"
+                  className="object-cover"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div>texttexttexttexttexttexttexttexttexttexttexttex</div>
+            </div>
+          </section>
+          <section className="grid gap-4">
+            <h1 className="text-4xl font-bold text-white">WORK</h1>
+            <div className="flex gap-4">
+              <div className="w-[200px]">
+                <Image
+                  alt="me"
+                  src="/about-me.jpg"
+                  className="object-cover"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div>texttexttexttexttexttexttexttexttexttexttexttex</div>
+            </div>
+          </section>
+          <section className="grid gap-4">
+            <h1 className="text-4xl font-bold text-white">SKATEBOARD</h1>
+            <div className="flex gap-4">
+              <div className="w-[200px]">
+                <Image
+                  alt="me"
+                  src="/about-me.jpg"
+                  className="object-cover"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div>texttexttexttexttexttexttexttexttexttexttexttex</div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
   )
 }
