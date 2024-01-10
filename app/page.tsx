576b2adbbc9ca818c6aab7a312406e33bf1f155c
kotaro1574 <shake1574@gmail.com>
2024-01-09 16:25:26 -0800

    update: work追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image"
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function IndexPage() {
   return (
     <main>
@@ -14,10 +21,10 @@ export default function IndexPage() {
         <div className="absolute inset-0 bg-black/50" />
       </div>
       <div className="container mx-auto px-4 py-6">
-        <div className="grid gap-6">
+        <div className="grid gap-12">
           <section className="grid gap-4">
             <h1 className="text-4xl font-bold text-white">ABOUT</h1>
-            <div className="flex gap-4">
+            <div className="flex justify-start gap-4">
               <div className="w-[200px]">
                 <Image
                   alt="me"
@@ -27,22 +34,28 @@ export default function IndexPage() {
                   height={200}
                 />
               </div>
-              <div>texttexttexttexttexttexttexttexttexttexttexttex</div>
+              <div className="">texttexttexttexttexttext</div>
             </div>
           </section>
           <section className="grid gap-4">
             <h1 className="text-4xl font-bold text-white">WORK</h1>
             <div className="flex gap-4">
-              <div className="w-[200px]">
-                <Image
-                  alt="me"
-                  src="/about-me.jpg"
-                  className="object-cover"
-                  width={200}
-                  height={200}
-                />
-              </div>
-              <div>texttexttexttexttexttexttexttexttexttexttexttex</div>
+              <Card className="max-w-[300px] rounded-none">
+                <CardContent className="p-4">
+                  <Image
+                    alt="me"
+                    src="/commit-timer.jpg"
+                    className="border object-cover"
+                    width={300}
+                    height={200}
+                  />
+                  <CardTitle className="mt-4">Commit Timer</CardTitle>
+                  <CardDescription className="mt-4">
+                    This application measures the commit time with a timer and
+                    displays it in a graph.
+                  </CardDescription>
+                </CardContent>
+              </Card>
             </div>
           </section>
           <section className="grid gap-4">
