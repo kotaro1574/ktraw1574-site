a4ee4b0f328e6e78e47d4bd9545f0eea871375e9
kotaro1574 <shake1574@gmail.com>
2024-01-11 14:11:59 -0800

    update: 文章考えたぞ

--- components/about.tsx
+++ components/about.tsx
@@ -4,19 +4,32 @@ export function About() {
   return (
     <section className="grid gap-4">
       <h1 className="text-4xl font-bold">ABOUT</h1>
-      <div className="flex justify-start gap-4">
-        <div className="w-[200px]">
+      <div className="grid justify-start gap-4 sm:flex">
+        <div className="w-[200px] shrink-0 md:w-[250px]">
           <Image
             alt="me"
             src="/about-me.jpg"
             className="object-cover"
-            width={200}
-            height={200}
+            width={250}
+            height={250}
           />
         </div>
-        <div className="">
-          Thank you from the bottom of my heart for visiting my microcosm of a
-          star-studded website.
+        <div>
+          <div className="text-2xl font-bold">Kotaro Sugita</div>
+          <p className="mt-2 text-muted-foreground">
+            I am a web application front-end developer who loves skateboarding
+            and the web. I am currently living in Vancouver, Canada with my wife
+            on a working holiday visa. Here I am skateboarding and developing
+            web applications. I like to commit.
+            <br />
+            I started as a front-end developer in 2019. Back then, I was honing
+            my skateboarding skills every day. However, I realized that I needed
+            physical rest in honing my skateboarding skills. Always wanting to
+            be committed to something, I was inspired by my brother to start
+            programming, which I am doing now.
+            <br />I will continue to be passionate about skateboarding and the
+            web.
+          </p>
         </div>
       </div>
     </section>
