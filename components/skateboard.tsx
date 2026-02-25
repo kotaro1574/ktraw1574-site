2d54c8959193862c38cdda04bf521c9738de33a9
kotaro1574 <shake1574@gmail.com>
2026-02-25 20:15:57 +0900

    update

--- components/skateboard.tsx
+++ components/skateboard.tsx
@@ -4,6 +4,8 @@ export function Skateboard() {
   return (
     <section className="grid gap-4">
       <h1 className="text-4xl font-bold">SKATEBOARD</h1>
+      <YouTubeEmbed videoid="7xQRXJhlDos" />
+      <YouTubeEmbed videoid="k4RFgn9Or9Y" />
       <YouTubeEmbed videoid="RsYLsAX1Ixw" />
     </section>
   )
